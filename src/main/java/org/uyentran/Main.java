8736f5c5843bb442db062ab6f8e31df7e3166a97
Uyen-Tran-jh <uyen.tran@journeyh.io>
2024-09-14 23:56:39 +0700

    Update BT4 _ Part3

--- src/main/java/org/uyentran/Main.java
+++ src/main/java/org/uyentran/Main.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Wait;
@@ -14,28 +15,42 @@ import java.time.Duration;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+
     static WebDriver driver = new ChromeDriver();
     static Wait<WebDriver> wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+
     public static void main(String[] args) throws InterruptedException {
-        //BT4
+        ChromeOptions chomeOptions = new ChromeOptions();
+        chomeOptions.enableBiDi();
+
+        ChromeDriver driver = new ChromeDriver(chomeOptions);
+        driver.manage().window().maximize();
+
         driver.get("https://globedr.com/signin");
         login();
 
-        //updateUserProfile();
         }
     public static void login()
     {
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div/input[@id=\"UserName\"]")));
-        WebElement txtEmail = driver.findElement(By.xpath("//div/input[@id=\"UserName\"]"));
-        txtEmail.sendKeys("+13434804615");
-        WebElement txtPassword = driver.findElement(By.xpath("//div/input[@id=\"Password\"]"));
-        txtPassword.sendKeys("12345678");
-        WebElement btnLogin = driver.findElement(By.xpath("//div/button[contains(@translate,\"signIn\")]"));
-        btnLogin.click();
-    }
-    public static void updateUserProfile()
-    {
+        By txtEmail = By.xpath("//input[@id='UserName']");
+        By dlCountry = By.xpath("//input[@dropdowntoggle]");
+        By txtPassword = By.xpath("//input[@id='Password']");
+        By btnLogin = By.xpath("//button[contains(@translate,'signIn')]");
 
+        wait.until(ExpectedConditions.visibilityOfElementLocated(txtEmail));
+        driver.findElement(txtEmail).sendKeys("+13434804615");
+        wait.until( d -> {
+            WebElement element = driver.findElement(dlCountry);
+            if(element!=null)
+            {
+                String txt = element.getText();
+                if (txt!=null && !txt.isEmpty()) return true;
+            }
+            return false;
+                }
+        );
+        driver.findElement(txtPassword).sendKeys("12345678");
+        driver.findElement(btnLogin).click();
     }
 
 
